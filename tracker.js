d68f2fd39819f3dd8f27808ad945789f73c288d2
tonyqiu123 <tonyqiu12345@gmail.com>
2023-09-13 19:10:21 -0400

    changed eventlistener from click to visibilitychange

--- tracker.js
+++ tracker.js
@@ -7,7 +7,7 @@ document.addEventListener('click', function () {
 
 var startTime = new Date().getTime();
 
-window.addEventListener('click', async function () {
+window.addEventListener('visibilitychange', async function () {
     let userCountry = await getUserCountry();
     let userDevice = detectDeviceType()
     let path = window.location.pathname;
